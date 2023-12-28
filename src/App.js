9897f33a1482f10a8da7445d122d3efc83debaab
Noah Shoap <112094300+noahshoap@users.noreply.github.com>
2023-12-28 11:52:20 -0500

    6 create project (#10)
    * feat(frontend): created project
    
    * deleted readme

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import jim from './logo.jpg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={jim} className="App-logo" alt="the holy father"/>
+        <p>
+          Check back later for all inventory related needs.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
