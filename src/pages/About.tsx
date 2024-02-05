74d4aab10c78a8983793b891a72d3fc840d1688c
Noah Shoap <112094300+noahshoap@users.noreply.github.com>
2024-02-05 09:24:24 -0500

    12 create basic pages for routing (#15)
    * feat(routing): installed react-router
    
    * feat(routing): added router
    
    * feat(routing): added pages
    
    * feat(about): basic about
    
    * feat(landing): basic landing
    
    * feat(business): added basic registration
    
    * added routes

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div id="aboutPage">
+            <h1>About Streamline</h1>
+            <p>Check back later for more information.</p>
+        </div>
+    );
+}
---
+++ src/pages/BusinessRegistration.tsx
@@ -0,0 +1,7 @@
+export default function BusinessRegistration() {
+    return (
+        <div id="businessRegistrationPage">
+            <h1>Register a Business</h1>
+        </div>
+    );
+}
