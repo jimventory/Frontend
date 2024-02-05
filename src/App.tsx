fa4e41d6dc1bd2fad18980093d671066643bdece
Noah Shoap <nshoap@vols.utk.edu>
2024-02-05 09:23:03 -0500

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import BusinessRegistration from './pages/BusinessRegistration';
+import Landing from './pages/Landing';
 
 function App() {
   return (
     <div className="App">
+        <div id="pageContainer">
+            <Routes>
+                    <Route path="/" Component={Landing} />
+                    <Route path="/register" Component={BusinessRegistration} />
+                    <Route path="/about" Component={About} />
+                </Routes>
+        </div>
     </div>
   );
 }
