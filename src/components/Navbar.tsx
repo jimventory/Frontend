2df4271148afa119ef6f288f2fddf1ea6e20cb1d
jhulen <112642997+jhulen@users.noreply.github.com>
2024-02-09 15:43:27 -0500

    added dark logo (#18)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom';
-import '../stylesheets/Navbar.css';
-import UserInformation from './UserInformation';
+import { Link } from "react-router-dom";
+import "../stylesheets/Navbar.css";
+import UserInformation from "./UserInformation";
+import logo from "../images/darkLogo.png";
 
 export default function Navbar() {
-    return (
-        <nav id="navbar">
-            <Link to="/"><h1>Streamline</h1></Link>
-            <ul>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/register">Register Business</Link></li>
-                <UserInformation/>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav id="navbar">
+      <Link to="/" className="logo">
+        <img src={logo} alt="Streamline logo" />
+      </Link>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register Business</Link>
+        </li>
+        <UserInformation />
+      </ul>
+    </nav>
+  );
 }
