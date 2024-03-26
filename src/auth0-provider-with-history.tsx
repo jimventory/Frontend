79d35da8dd6f0f56a08cbe61216830acade24f47
Noah Shoap <nshoap@vols.utk.edu>
2024-03-26 16:06:07 -0400

    feat(auth): reworking auth

---
+++ src/auth0-provider-with-history.tsx
@@ -0,0 +1,40 @@
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import React, { PropsWithChildren } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface Auth0ProviderWithConfigProps {
+  children: React.ReactNode;
+}
+
+export const Auth0ProviderWithHistory = ({
+  children,
+}: PropsWithChildren<Auth0ProviderWithConfigProps>): JSX.Element | null => {
+  const navigate = useNavigate();
+
+  const domain="dev-85peonfew8syv3it.us.auth0.com";
+  const clientId="FdVf3LoTNyrGbRR5wSdZnIDrAo72jUS1";
+  const redirectUri=window.location.origin;
+  const audience = "https://localhost:7079/api" 
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || window.location.pathname);
+  };
+
+  if (!(domain && clientId && redirectUri && audience)) {
+    return null;
+  }
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        audience: audience,
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
