a3bfc00df0e2aa2ba28536263c0a8a5d074b8864
Joseph Wehby <josephw31@hotmail.com>
2024-03-26 15:58:51 -0400

    done

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,8 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import '../stylesheets/LoginLogoutButton.css';
 
 export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <button className="loginlogout-button" onClick={() => loginWithRedirect()}>Log In</button>;
 }
