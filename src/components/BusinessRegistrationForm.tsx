228cfe12b4b451ee2d2952bd30c9996b5a9378bb
jhulen <112642997+jhulen@users.noreply.github.com>
2024-03-30 17:15:06 -0400

    made about, did styling (#49)

--- src/components/BusinessRegistrationForm.tsx
+++ src/components/BusinessRegistrationForm.tsx
@@ -1,80 +1,89 @@
-import "../stylesheets/BusinessRegistration.css"
-import React, { ChangeEvent, useState } from 'react';
-import { useAuth0 } from "@auth0/auth0-react"
+import "../stylesheets/BusinessRegistration.css";
+import React, { ChangeEvent, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function BusinessRegistrationForm() {
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const registrationApi: string =
+    "https://localhost:7079/api/business/register";
+  const { user, getAccessTokenSilently } = useAuth0();
 
-    const [name, setName] = useState('');
-    const [location, setLocation] = useState('');
-    const registrationApi: string = "https://localhost:7079/api/business/register";
-    const { user, getAccessTokenSilently } = useAuth0();
+  async function registerBusiness() {
+    if (user === null) return;
 
-    async function registerBusiness() {
-        if (user === null)
-            return;
+    if (user?.sub === undefined) return;
 
-        if (user?.sub === undefined)
-            return;
+    console.log(user?.sub);
 
-        console.log(user?.sub);
+    const idString = user?.sub.split("|");
+    const idNumString = idString[1].substring(idString[1].length - 8);
+    console.log(idNumString);
+    const idNumUint = parseInt(idNumString, 16);
 
-        const idString = user?.sub.split("|");
-        const idNumString = idString[1].substring(idString[1].length - 8);
-        console.log(idNumString);
-        const idNumUint = parseInt(idNumString, 16);
+    console.log(`Id is ${idNumUint}`);
 
-        console.log(`Id is ${idNumUint}`);
+    let formData = {
+      Name: name,
+      Location: location,
+      Id: idNumUint,
+    };
 
-        let formData = {
-            "Name" : name,
-            "Location": location,
-            Id: idNumUint,
-        };
+    console.log(JSON.stringify(formData));
 
-        console.log(JSON.stringify(formData));
+    fetch(registrationApi, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.ok) alert("Registered.");
+        else throw new Error("Couldn't register");
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }
 
-        fetch(registrationApi, {
-            method: "PUT",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formData)
-        })
-        .then(response => {
-            console.log(response);
-            if (response.ok)
-                alert("Registered.");
-            else
-                throw new Error("Couldn't register");
-        })
-        .catch(error => {
-            alert(error);
-        });
-    }
+  function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
 
+  function handleLocationChange(event: ChangeEvent<HTMLInputElement>) {
+    setLocation(event.target.value);
+  }
 
-    function handleNameChange(event: ChangeEvent<HTMLInputElement>) {
-        setName(event.target.value);
-    }
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-    function handleLocationChange(event: ChangeEvent<HTMLInputElement>) {
-        setLocation(event.target.value);
-    }
+    await registerBusiness();
+  }
 
-    async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-
-        await registerBusiness();
-    }
-
-    return (
-        <form id="businessRegistrationForm" onSubmit={handleSubmit}>
-            <h2>Register</h2>
-            <label htmlFor="businessName">Name</label>
-            <input type="text" id="businessName" name="businessName" onChange={handleNameChange}></input>
-            <label htmlFor="businessLocation">Location</label>
-            <input type="text" id="businessLocation" name="businessLocation" onChange={handleLocationChange}></input>
-            <input type="submit" value="Register" className="registerButton"></input>
-        </form>
-    )
+  return (
+    <form id="businessRegistrationForm" onSubmit={handleSubmit}>
+      <h2>Register your business to get started.</h2>
+      <label htmlFor="businessName">Business Name</label>
+      <input
+        type="text"
+        id="businessName"
+        name="businessName"
+        onChange={handleNameChange}
+      ></input>
+      <label htmlFor="businessLocation">Business Location</label>
+      <input
+        type="text"
+        id="businessLocation"
+        name="businessLocation"
+        onChange={handleLocationChange}
+      ></input>
+      <input
+        type="submit"
+        value="R E G I S T E R"
+        className="registerButton"
+      ></input>
+    </form>
+  );
 }
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,93 @@
+/* Hello! You have found the About Jimventory
+page! I hope you like it! */
+
+import React from "react";
+import { Link } from "react-router-dom";
+import "../stylesheets/About.css";
+
 export default function About() {
-    return (
-        <div id="aboutPage">
-            <h1>About Streamline</h1>
-            <p>Check back later for more information.</p>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="rectangle left">
+        <p>
+          <br /> S A Y &nbsp;&nbsp; G O O D - B Y E &nbsp;&nbsp; T O
+          &nbsp;&nbsp; S P R E A D S H E E T S . . . <br /> <br />
+        </p>
+      </div>
+      <div className="rectangle right">
+        <p>
+          S T R E A M |<strong> L I N E</strong> <br /> <br />
+          An innovative new tool to streamline your company's inventory,
+          designed for simplicity and affordability
+          <br />
+          <br />
+          Specificially created for small business, StreamLine provides all the
+          tools needed to take your company's inventory to the next level
+        </p>
+      </div>
+      <div className="rectangle left">
+        <p>
+          A C C E S S &nbsp;&nbsp;A N Y W H E R E , &nbsp;&nbsp;A N Y
+          &nbsp;&nbsp;T I M E.
+          <br />
+          <br />
+          StreamLine can be accessed on any platform with a web browser <br />
+          <br />
+          Manage inventory on your computer, on your phone, or let employeess
+          update stock with a company tablet
+        </p>
+      </div>
+      <div className="rectangle right">
+        <p>
+          E A S Y &nbsp;&nbsp; T O &nbsp;&nbsp; U S E.
+          <br />
+          <br />
+          Low effort inventory importing
+          <br />
+          <br />
+          Track product shipping, recieve low stock notifications, see sales
+          trends, and more
+        </p>
+      </div>
+      <div className="rectangle left">
+        <p>
+          A F F O R D A B L E.
+          <br />
+          <br />
+          StreamLine offers only one tier of membership
+          <br />
+          <br />
+          One low monthly subscription fee gives you access to the entire
+          StreamLine suite for a fraction of competitor costs
+        </p>
+      </div>
+      <div className="rectangle right">
+        <p>
+          M A D E &nbsp;&nbsp; F O R &nbsp;&nbsp; Y O U R &nbsp;&nbsp; S M A L L
+          &nbsp;&nbsp; B U S I N E S S.
+          <br />
+          <br />
+          Flat rate subscription fee that won't break the bank
+          <br />
+          <br />
+          Simple, fast, intuitive
+        </p>
+      </div>
+      <div className="rectangle left">
+        <p>
+          W H A T &nbsp;&nbsp; A R E &nbsp;&nbsp; Y O U &nbsp;&nbsp; W A I T I N
+          G &nbsp;&nbsp; F O R ?
+          <br />
+          <br />
+          <div className="landing-buttons">
+            <Link to="/register">
+              <button className="button">
+                G E T&nbsp;&nbsp;S T A R T E D.{" "}
+              </button>
+            </Link>
+          </div>
+        </p>
+      </div>
+    </div>
+  );
 }
