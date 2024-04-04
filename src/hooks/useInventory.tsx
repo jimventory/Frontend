a99d2121bf004492a8e676011037c596307f2444
Noah Shoap <nshoap@vols.utk.edu>
2024-04-04 14:39:35 -0400

    still working on it

--- src/hooks/useInventory.tsx
+++ src/hooks/useInventory.tsx
@@ -1,6 +1,14 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { useState, useEffect } from 'react';
-import { Item } from "../abstractions/Item"
+// import { Item } from "../abstractions/Item"
+
+interface Item {
+  id: number;
+  name: string;
+  about: string;
+  price: number;
+  quantity: number;
+}
 
 export default function useInventory() {
     const { getAccessTokenSilently } = useAuth0();
@@ -24,11 +32,20 @@ export default function useInventory() {
 
             const response = await fetch(api, options);
 
+            console.log("Received response.");
+
             if (response.ok === false)
                 throw new Error("Failed to fetch inventory.")
 
+            console.log("Response was ok.");
+
+            console.log(response);
+
             const data = await response.json();
             setItems(data);
+
+            console.log(`Set items to be ${data}`);
+            console.log(items);
         };
 
         initializeInventory();
