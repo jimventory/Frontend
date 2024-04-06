7bf9ec966dd18b8cd1bb4330be3db41883b2915b
Noah Shoap <nshoap@vols.utk.edu>
2024-04-05 21:07:21 -0400

    adding selectedItem to context

--- src/contexts/InventoryContext.tsx
+++ src/contexts/InventoryContext.tsx
@@ -3,8 +3,12 @@ import { Item } from "../abstractions/Item";
 
 const emptyArray: Item[] = [];
 
-const noSetter: Dispatch<SetStateAction<Item[]>> = (value: SetStateAction<Item[]>) => {
+const noItemArraySetter: Dispatch<SetStateAction<Item[]>> = (value: SetStateAction<Item[]>) => {
     console.log(`No setter has been set.  A setter was called with new value ${value} but it will not be saved.`);
 };
 
-export const InventoryContext = createContext({items: emptyArray, setItems: noSetter});
+const noItemSetter: Dispatch<SetStateAction<Item>> = (value: SetStateAction<Item>) => {
+    console.log(`No setter has been set.  A setter was called with new value ${value} but it will not be saved.`);
+};
+
+export const InventoryContext = createContext({items: emptyArray, setItems: noItemArraySetter, selectedItem: null, setSelectedItem: noItemSetter});
