33d3854685343aa8737f25d8bf0882ef0fccc16b
Noah Shoap <nshoap@vols.utk.edu>
2024-04-08 17:09:23 -0400

    fix(business): stop crash if not logged in

--- src/hooks/useBusinessId.tsx
+++ src/hooks/useBusinessId.tsx
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export default function useBusinessId() {
   const { user } = useAuth0();
-
+  try { 
   if (user === null) throw new Error("User object is null");
 
   if (user?.sub === undefined) throw new Error("user.sub is undefined");
@@ -14,4 +14,8 @@ export default function useBusinessId() {
   const userIdNum = parseInt(userNumString, 16);
 
   return userIdNum;
+  } catch (e) {
+    console.log("Failed to get business Id.");
+    return null;
+  }
 }
