c0c522e4bbd3c459a012883e9b28bcc2a66a92d5
Joseph Wehby <josephw31@hotmail.com>
2024-04-15 09:33:21 -0400

    the most beautiful drop down menu ever (#64)

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,6 +1,5 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import "../stylesheets/LoginLogoutButton.css";
 
 export default function LogoutButton() {
   const { logout } = useAuth0();
