ae6fcb939bede2772ee6df1990222d0a1b4314f1
Noah Shoap <nshoap@vols.utk.edu>
2024-04-18 20:04:21 -0400

    search

--- src/components/InventoryList.tsx
+++ src/components/InventoryList.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { InventoryContext } from "../contexts/InventoryContext";
 import AddItemButton from "./AddItemButton";
+import SearchBar from "./SearchBar";
 
 export default function InventoryList() {
   const { items, setSelectedItem } = useContext(InventoryContext);
@@ -8,6 +9,7 @@ export default function InventoryList() {
   return (
     <div id="inventory-list">
       <h1>Inventory List</h1>
+      <SearchBar/>
       <ul>
         {items.map((item, index) => (
           <li key={index} onClick={() => setSelectedItem(item)}>
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,12 @@
+import "../stylesheets/SearchBar.css";
+
+export default function SearchBar() {
+
+    return (
+    <div id="searchBar">
+        <button id="searchFilter">Filters</button>
+        <input id="search" placeholder="Search for an item"/>
+    </div>
+    );
+
+};
