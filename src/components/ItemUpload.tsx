6c272a5d873d0d7607bd47f0662dd523ddcfe0b2
Vincent Broda <101155730+vbroda17@users.noreply.github.com>
2024-04-23 01:09:28 -0400

    fixed warning

--- src/components/ItemUpload.tsx
+++ src/components/ItemUpload.tsx
@@ -1,5 +1,5 @@
 import { getFullPath, API_ROUTES } from "../apis/inventory";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function ItemUpload() {
