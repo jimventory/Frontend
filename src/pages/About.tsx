f0d12224c8f1f61a1fd415a4a1de9c02d3a122aa
josephwehby <josephw31@hotmail.com>
2024-04-27 13:09:07 -0400

    removed unessecary link import

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,6 @@ page! I hope you like it! */
 
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Link } from "react-router-dom";
 import "../stylesheets/About.css";
 
 export default function About() {
